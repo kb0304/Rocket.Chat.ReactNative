385c4fc22b86cc4590251a38635a3dda7f022856
Diego Mello <diegolmello@gmail.com>
2018-01-19 11:25:43 -0200

    .

--- app/views/RoomView/Header/index.js
+++ app/views/RoomView/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text, View, Platform, TouchableOpacity } from 'react-native';
+import { Text, View, Platform, TouchableOpacity, WebView } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { HeaderBackButton } from 'react-navigation';
+import Modal from 'react-native-modal';
 
 import realm from '../../../lib/realm';
 import Avatar from '../../../containers/Avatar';
@@ -31,7 +32,8 @@ export default class extends React.PureComponent {
 		super(props);
 		this.state = {
 			room: {},
-			roomName: props.navigation.state.params.name
+			roomName: props.navigation.state.params.name,
+			isModalVisible: false
 		};
 		this.rid = props.navigation.state.params.room.rid;
 		this.room = realm.objects('subscriptions').filtered('rid = $0', this.rid);
@@ -60,6 +62,16 @@ export default class extends React.PureComponent {
 	};
 
 	isDirect = () => this.state.room && this.state.room.t === 'd';
+
+	toggleModal() {
+		this.setState({ isModalVisible: !this.state.isModalVisible });
+	}
+
+	joinMconf = async() => {
+		const result = await fetch('https://live11-kms.dev.mconf.com/bigbluebutton/api/create?allowStartStopRecording=true&attendeePW=ap&autoStartRecording=false&meetingID=random-1014327&moderatorPW=mp&name=random-1014327&record=false&voiceBridge=74831&welcome=%3Cbr%3EWelcome+to+%3Cb%3E%25%25CONFNAME%25%25%3C%2Fb%3E%21&checksum=f9dd2290ebec40ff40c66efcccb43e282814e17f')
+		// const join = await fetch('https://live11-kms.dev.mconf.com/bigbluebutton/api/join?fullName=User+889230&meetingID=random-1014327&password=mp&redirect=true&checksum=7aafe8f21961b85fe296923a855ad0223cf7ac2f');
+		this.toggleModal();
+	}
 
 	renderLeft = () => (<HeaderBackButton
 		onPress={() => {
@@ -110,7 +122,7 @@ export default class extends React.PureComponent {
 		<View style={styles.right}>
 			<TouchableOpacity
 				style={styles.headerButton}
-				onPress={() => {}}
+				onPress={() => this.joinMconf()}
 				accessibilityLabel='Room actions'
 				accessibilityTraits='button'
 			>
@@ -126,11 +138,24 @@ export default class extends React.PureComponent {
 
 	render() {
 		return (
-			<View style={styles.header}>
-				{this.renderLeft()}
-				{this.renderTitle()}
-				{this.renderRight()}
-			</View>
+			[
+				<View style={styles.header}>
+					{this.renderLeft()}
+					{this.renderTitle()}
+					{this.renderRight()}
+				</View>,
+				<Modal
+					isVisible={this.state.isModalVisible}
+					supportedOrientations={['portrait', 'landscape']}
+					style={{ alignItems: 'center' }}
+					onBackdropPress={() => this.hideModal()}
+				>
+					<WebView
+						source={{ uri: 'https://live11-kms.dev.mconf.com/bigbluebutton/api/join?clientURL=https://live11-kms.dev.mconf.com/html5client/join&fullName=Guilherme+Gazzo&meetingID=random-6822671&password=ap&redirect=true&checksum=f7d0be999fb9be7b2a7352d12904334ca82c849d' }}
+						style={{ width: '100%', height: '100%' }}
+					/>
+				</Modal>
+			]
 		);
 	}
 }
